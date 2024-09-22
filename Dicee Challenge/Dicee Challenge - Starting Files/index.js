0cb4483bf6a65c894f58727971d53cd8c295d8a2
Drăghici Andrei <161716557+DraghiciAndrei1307@users.noreply.github.com>
2024-09-22 22:22:16 +0300

    Added a button

--- Dicee Challenge/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge/Dicee Challenge - Starting Files/index.js
@@ -1,7 +1,5 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
-document.querySelector("img.img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
 var randomNumber2 = Math.floor(Math.random()*6)+1;
-document.querySelector("img.img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
 
 function changeH1Text(){ 
 
@@ -13,4 +11,19 @@ function changeH1Text(){
        document.querySelector(".container > h1").innerHTML = "Draw";
 }
 
-changeH1Text();
+function changeDice(){
+
+    document.querySelector("img.img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+    document.querySelector("img.img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+}
+play();
+function play(){
+
+    changeH1Text();
+    changeDice();
+
+}
+
+
+
+document.querySelector(".flex-container > .button").onclick();
