29b21e8d92db73d18cd5668ef7a7d8b8bdacb44e
Drăghici Andrei <161716557+DraghiciAndrei1307@users.noreply.github.com>
2024-09-22 21:16:27 +0300

    Section 16: The Document Object Model(DOM)

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,2 +1,6 @@
 document.querySelector("ul").lastElementChild.innerHTML = "Andrei Draghici";
 document.querySelector("li > a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+
+
+document.querySelector("body > h1").classList.add("huge");
