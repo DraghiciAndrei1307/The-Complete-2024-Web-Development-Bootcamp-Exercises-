42f052e88909ba5d2c04bb599748f5cbe1cefc77
Drăghici Andrei <161716557+DraghiciAndrei1307@users.noreply.github.com>
2024-09-26 21:47:08 +0300

    Finished jQuery section

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,18 @@
  $(document).ready(function(){
     $("h1").css("color", "red");
+
+    $(document).keydown(function(event){
+        $("h1").text(event.key);
+    });
+
+    $("h1").on("mouseover", function(){
+        $("h1").css("color", "purple");
+    });
+    
+    $("h1").before("<button>New</button>");
+
+    $("h1").prepend("<em>Hello</em>");
+
 });
 
 
